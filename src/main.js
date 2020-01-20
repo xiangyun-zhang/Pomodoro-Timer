3f52cd63de5c986659409951512c0a8e01b8f783
Arnold <register@ixiangyun.me>
2020-01-20 14:59:14 +0800

    add font-awsome, set eslint rules no-unsued-vars into as off

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import $ from 'jquery'
-import 'bootstrap/dist/js/ bootstrap.bundle.min.js '
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
