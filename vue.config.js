a158a3af4bd5e4da9ce532320fbe6c3cc0d987ce
Arnold <register@ixiangyun.me>
2020-02-02 01:44:53 +0800

    modify output dir into docs folder

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const webpack = require("webpack");
 
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/Pomodoro-Timer/'
+		: '/',
+	outputDir:'docs',
 	configureWebpack: {
 		plugins: [
 			new webpack.ProvidePlugin({	
